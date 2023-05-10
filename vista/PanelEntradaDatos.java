50a61f61525d45dd5477be7d7c07cffa6cf581cb
Olarte18 <alejolarte18@gmail.com>
2023-05-10 12:01:51 -0500

    label

--- vista/PanelEntradaDatos.java
+++ vista/PanelEntradaDatos.java
@@ -1,10 +1,13 @@
 package vista;
 
 import java.awt.Color;
+import java.awt.TextField;
+
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class PanelEntradaDatos extends JPanel
@@ -12,8 +15,9 @@ public class PanelEntradaDatos extends JPanel
     //----------------------
     // Atributos
     //----------------------
-    private JLabel lbOperador;
+    private JLabel lbOperador, lbNum, lbValor,lbCant ;
     private JComboBox cbOperador;
+    private TextField txNum,txValor, txCant; 
 
     //----------------------
     // Metodos
@@ -26,15 +30,26 @@ public class PanelEntradaDatos extends JPanel
 
         //Crear y agregar etiqueta empleados
         lbOperador = new JLabel("Operadores ", JLabel.RIGHT);
-        lbOperador.setBounds(10,90,100,20);
+        lbOperador.setBounds(0,110,100,20);
         this.add(lbOperador);
-
+        //Crear y agregar texto empleados
+        lbNum = new JLabel("Numero celular: ", JLabel.RIGHT);
+        lbNum.setBounds(10,20,100,20);
+        this.add(lbNum);
+        //Crear y agregar etiqueta empleados
+        lbValor = new JLabel("Valor minuto ");
+        lbValor.setBounds(0,50,100,20);
+        this.add(lbValor);
+        //Crear y agregar etiqueta empleados
+        lbCant = new JLabel("Num Minutos ", JLabel.RIGHT);
+        lbCant.setBounds(10,80,100,20);
+        this.add(lbCant);
         //Crear y agregar combo lista Empleados
         cbOperador = new JComboBox();
 		cbOperador.addItem("Claro");
 		cbOperador.addItem("Wom");
 		cbOperador.addItem("tigo");
-        cbOperador.setBounds(100,90,260,20);
+        cbOperador.setBounds(100,110,260,20);
         this.add(cbOperador);
 
         //Borde y titulo del panel
